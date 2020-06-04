3d0fa3c3a3edd52c46f3e1cd71caa19b415c2501
dalon.zhang <dalon.zhang@rock-chips.com>
2020-06-04 20:21:30 +0800

    rkisp_parser_demo: parse xml and generate bin
    Change-Id: Ib9af105410e4f3dd374fd98f32b6430d210ec69a
    Signed-off-by: dalon.zhang <dalon.zhang@rock-chips.com>

---
+++ rkisp_parser_demo/demo/rkisp_parse.cpp
@@ -0,0 +1,45 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <stdio.h>
+#include "RkAiqCalibDb.h"
+
+using namespace RkCam;
+#define CALIBDB_PATH_LEN      128
+
+static void getFilePathName(char* iqFile, char *location)
+{
+    char dir[CALIBDB_PATH_LEN];
+    char name[CALIBDB_PATH_LEN];
+    char *srcfile = strdup(iqFile);
+    char *pstart = strrchr(srcfile,'/');
+    char *pend = strrchr(srcfile,'.');
+    *pend = '\0';
+    strcpy(name, pstart+1);
+    sprintf(location, "%s.bin", srcfile);
+    free(srcfile);
+    printf("calibdb file is %s",location);
+}
+
+static bool removeCalibBinIfExist(char* iqFile){
+    char path[CALIBDB_PATH_LEN];
+
+    getFilePathName(iqFile, path);
+    if (0==access(path, F_OK))
+        remove(path);
+}
+
+int main(int argc, char **argv)
+{
+    char config_file[CALIBDB_PATH_LEN];
+    strcpy(config_file, argv[1]);
+    removeCalibBinIfExist(config_file);
+    RkAiqCalibDb::createCalibDb(config_file);
+    RkAiqCalibDb::releaseCalibDb();
+    return 0;
+}
